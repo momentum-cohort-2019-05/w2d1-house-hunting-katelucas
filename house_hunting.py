04ed1c137ce1dc24db61342703a4f453336ca055
katelucas <lucas.katie@gmail.com>
2019-06-04 14:35:16 -0400

    finished project

--- house_hunting.py
+++ house_hunting.py
@@ -1,9 +1,18 @@
-annual_salary_as_str = input("What is your annual salary? ")
-portion_saved_as_str = input("What portion would you like to save? ")
-dream_home_as_str = input("How much does your dream home cost? ")
+annual_salary = int(input("What is your annual salary? "))
+portion_saved = float(input("What portion, in decimal form, would you like to save? "))
+total_cost = int(input("How much does your dream home cost? "))
+goal_down_payment = int(total_cost * 0.25)
+monthly_payment = int(portion_saved * annual_salary/12)
+r = .04
+current_savings = 0
+number_of_months = 0
 
-portion_down_payment = str(dream_home_as_str * portion_saved_as_str)
+while current_savings < goal_down_payment:
+    number_of_months = number_of_months + 1
+    current_savings = current_savings + monthly_payment + (current_savings*r/12)
 
-print(portion_down_payment)
+
+print(number_of_months)
+
 
 
